9b590c6e9b7d99ac05880a4fe88d6d2b8715a271
isaac k. kimura <isaackimura74@gmail.com>
2022-09-30 11:11:48 +0300

    Done Desinging the home, single,and write pages

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,107 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
+
+import Delete from "../img/delete.png";
+import Edit from "../img/edit.png";
+
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className='single-page'>
+      <div className='content'>
+        <img
+          src='https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt='single page'
+          srcset=''
+        />
+        <div className='user'>
+          <img
+            src='https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+            alt='user'
+            srcset=''
+          />
+          <div className='info'>
+            <span>Isaac</span>
+            <p>Posted two days ago</p>
+          </div>
+          <div className='edit'>
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt='edit' srcset='' />
+            </Link>
+            <img src={Delete} alt='delete' srcset='' />
+          </div>
+        </div>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h1>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut feugiat
+            mi non sagittis laoreet. Fusce sed bibendum ante. Morbi dapibus
+            luctus justo, id auctor lorem maximus vel. Mauris sed blandit quam.
+            Aliquam convallis nisl ut diam sollicitudin, ut eleifend quam
+            eleifend. Praesent porttitor diam metus, ut tempor tellus maximus
+            non. Duis
+          </p>
+          <p>
+            fringilla erat non enim posuere, vel aliquam urna eleifend. Praesent
+            quis lacus quis magna feugiat scelerisque. Maecenas scelerisque
+            nulla sed pulvinar venenatis. In vehicula luctus neque vel porta.
+            Suspendisse ac sem vitae mi lacinia iaculis. Sed bibendum mi ut odio
+          </p>
+          <p>
+            accumsan rutrum. Nunc rhoncus ac est eu tincidunt. Vivamus sed
+            tempor nulla. Pellentesque est enim, egestas eu risus eget,
+            dignissim egestas ligula. Integer turpis mauris, pulvinar ut
+            vehicula in, bibendum eu mi. Maecenas in mauris ut est hendrerit
+            imperdiet a ac erat. Nulla pharetra rutrum efficitur. Cras sed
+            pretium est, nec auctor erat. Aenean imperdiet euismod est non
+            lobortis. Pellentesque faucibus vel
+          </p>
+          <p>
+            leo ac placerat. Nullam pharetra tristique lorem, eget tempus leo
+            lacinia id. Vivamus bibendum ante arcu, id feugiat massa fermentum
+            quis. Etiam quis vehicula augue. Mauris nec bibendum lectus, nec
+            sodales odio. Nam sem odio, scelerisque a augue eu, finibus ornare
+            nibh. Duis viverra, diam nec finibus pulvinar, lorem erat sagittis
+            quam, in pretium tortor ligula sed purus. Pellentesque posuere dolor
+          </p>
+          <p>
+            vitae odio accumsan porttitor. Duis tincidunt interdum pellentesque.
+            Quisque ullamcorper massa leo, sed venenatis nunc hendrerit sed.
+            Donec tellus metus, eleifend ut leo id, posuere commodo leo. Morbi
+            finibus accumsan tristique. Pellentesque dapibus diam id lacus
+            gravida, sed varius arcu gravida. Fusce tempor, odio et ornare
+            malesuada, lorem neque eleifend felis, quis tincidunt augue sapien
+            at metus. Suspendisse molestie diam nisl, at efficitur quam bibendum
+            quis. Maecenas eu eros sollicitudin, semper mi nec, ullamcorper
+            quam. Nullam euismod interdum felis sit amet placerat. Fusce
+            placerat interdum dui quis vestibulum. Pellentesque mi arcu,
+            sollicitudin vel vulputate non, pretium luctus mi. Nam luctus nec
+            sem ac maximus. Suspendisse potenti.
+          </p>
+          <p>
+            Fusce sed viverra est. Mauris vitae faucibus nibh, semper bibendum
+            erat. Vivamus facilisis quam diam, sed iaculis nunc hendrerit vel.
+            Nulla facilisi. Mauris aliquam ullamcorper tempus. Phasellus ut ante
+            consectetur erat molestie gravida. Sed pulvinar posuere felis, quis
+            egestas erat aliquam vitae. Ut at felis vel felis laoreet semper
+            finibus vel ipsum. In rutrum tristique ante, non ullamcorper sapien
+          </p>
+          <p>
+            condimentum nec. Curabitur vitae velit quis lorem iaculis porttitor.
+            Mauris vel volutpat tortor. Praesent quis posuere justo, quis dictum
+            dui. Duis porttitor mi id ligula congue, at facilisis nisi
+            vestibulum. In quis vehicula lectus. Cras facilisis pulvinar erat,
+            sed finibus ligula feugiat vel. Cras ligula odio, vestibulum eget
+            dui rutrum, ullamcorper gravida lorem.
+          </p>
+        </p>
+      </div>
+      <div className='menu'>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Single;
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  return <div>Write</div>;
+  const [value, setValue] = useState("");
+
+  console.log(value);
+
+  return (
+    <div className='add'>
+      <div className='content'>
+        <input type='text' placeholder='Title' />
+        <div className='editorContainer'>
+          <ReactQuill className="editor" theme='snow' value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className='menu'>
+        <div className='items'>
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type='file' id='updateImage' />
+          <label className="file" htmlFor='updateImage'>Upload Image</label>
+          <div className='buttons'>
+            <button>Save the draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className='items'>
+          <h1>Category</h1>
+          <div className="cat">
+            <input type='radio' name='cat' value='NodeJS' id='node-js' />
+            <label htmlFor='node-js'>NodeJS</label>
+          </div>
+          <div className="cat">
+            <input type='radio' name='cat' value='ReactJS' id='react-js' />
+            <label htmlFor='react-js'>ReactJS</label>
+          </div>
+          <div className="cat">
+            <input type='radio' name='cat' value='ExpressJS' id='express-js' />
+            <label htmlFor='express-js'>ExpressJS</label>
+          </div>
+
+          <div className="cat">
+            <input type='radio' name='cat' value='HTML' id='html' />
+            <label htmlFor='html'>HTML</label>
+          </div>
+          <div className="cat">
+            <input type='radio' name='cat' value='CSS' id='css' />
+            <label htmlFor='css'>CSS</label>
+          </div>
+          <div className="cat">
+            <input type='radio' name='cat' value='SEO' id='seo' />
+            <label htmlFor='seo'>SEO</label>
+          </div>
+          <div className="cat">
+            <input type='radio' name='cat' value='Design' id='design' />
+            <label htmlFor='design'>Design</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
